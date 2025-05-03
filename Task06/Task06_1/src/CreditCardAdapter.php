220da1a12ec66639714c77778ad32eb6901dc441
maksim <->
2025-05-03 22:15:42 +0300

    Task06

--- Task06/Task06_1/src/CreditCardAdapter.php
+++ Task06/Task06_1/src/CreditCardAdapter.php
@@ -4,4 +4,14 @@ namespace App;
 
 class CreditCardAdapter implements PaymentAdapterInterface
 {
+    private CreditCard $card;
+
+    public function __construct(CreditCard $card)
+    {
+        $this->card = $card;
+    }
+    public function collectMoney($amount): bool
+    {
+        return $this->card->authorizeTransaction($amount) ? true : false;
+    }
 }
--- Task06/Task06_1/src/PayPalAdapter.php
+++ Task06/Task06_1/src/PayPalAdapter.php
@@ -4,4 +4,14 @@ namespace App;
 
 class PayPalAdapter implements PaymentAdapterInterface
 {
+    private PayPal $payPal;
+
+    public function __construct(PayPal $payPal)
+    {
+        $this->payPal = $payPal;
+    }
+    public function collectMoney($amount): bool
+    {
+        return $this->payPal->transfer("onafixed@mail.da", $amount) ? true : false;
+    }
 }
